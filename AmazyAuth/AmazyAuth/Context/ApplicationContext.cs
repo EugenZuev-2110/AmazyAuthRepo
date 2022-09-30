5ced267bbcbebc758a9bfeab0b9d691b38e7b591
Eugen <zhenya.zuev2110@gmail.com>
2022-09-30 03:26:14 +0300

    Person entity was added. Context was added

---
+++ AmazyAuth/AmazyAuth/Context/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AmazyAuth.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=localhost;user=root;password=322398;database=amazyauthdb;",
+                new MySqlServerVersion(new Version(8, 0, 30)));
+        }
+    }
+}
